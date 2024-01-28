4dcbc37ec733482b46001782e51681843458f51d
MaxData <max@workhere.org>
2024-01-28 14:30:09 -0500

    c

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import DashboardClient from './dashboard-client';
 import prisma from '@/utils/prisma';
-import { currentUser } from '@clerk/nextjs';
-import type { User } from '@clerk/nextjs/api';
+import { User, currentUser } from '../../utils/user';
 
 export default async function Page() {
   const user: User | null = await currentUser();
